491f2bb3834793911fe8f73ef271efa2bb50bbe2
CluelessWizard <tremmel.david@hotmail.com>
2020-07-13 14:24:52 +0200

    Specified data object

--- src/api/index.js
+++ src/api/index.js
@@ -4,7 +4,21 @@ const url = "https://covid19.mathdro.id/api";
 
 export const fetchData = async () => {
   try {
-    const response = await axios.get(url);
-    return response;
+    // Method one
+    // const { data } = await axios.get(url);
+    // const modifiedData = {
+    //   confirmed: data.confirmed,
+    //   recovered: data.recovered,
+    //   deaths: data.deaths,
+    //   lastUpdate: data.lastUpdate,
+    // };
+
+    // Method two
+
+    const {
+      data: { confirmed, recovered, deaths, lastUpdate },
+    } = await axios.get(url);
+
+    return { confirmed, recovered, deaths, lastUpdate };
   } catch (error) {}
 };
