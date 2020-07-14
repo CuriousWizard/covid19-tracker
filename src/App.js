ac2acc8cc521aad99157690b2df3ceb215744f5f
CluelessWizard <tremmel.david@hotmail.com>
2020-07-14 09:46:07 +0200

    Changing cards according to picked country

--- src/App.js
+++ src/App.js
@@ -22,18 +22,20 @@ class App extends React.Component {
   }
 
   handleCountryChange = async (country) => {
-    console.log(country);
+    const fetchedData = await fetchData(country);
+    //console.log(country);
+    this.setState({ data: fetchedData, country: country });
   };
 
   render() {
-    const { data } = this.state;
+    const { data, country } = this.state;
 
     return (
       <div className={styles.container}>
         <h1>COVID-19 Tracker</h1>
         <Cards data={data} />
         <CountryPicker handleCountryChange={this.handleCountryChange} />
-        <Chart />
+        <Chart data={data} country={country} />
       </div>
     );
   }
--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,13 @@ import axios from "axios";
 
 const url = "https://covid19.mathdro.id/api";
 
-export const fetchData = async () => {
+export const fetchData = async (country) => {
+  let changeableUrl = url;
+
+  if (country && country !== "global") {
+    changeableUrl = `${url}/countries/${country}`;
+  }
+
   try {
     // Method one
     // const { data } = await axios.get(url);
@@ -17,7 +23,7 @@ export const fetchData = async () => {
 
     const {
       data: { confirmed, recovered, deaths, lastUpdate },
-    } = await axios.get(url);
+    } = await axios.get(changeableUrl);
 
     return { confirmed, recovered, deaths, lastUpdate };
   } catch (error) {
